35e9212054c169b6f491cbb950dde93666c1d390
Evandro <evandrolt@gmail.com>
2019-03-31 15:31:13 -0300

    Inclusão de print

--- src/main/java/com/etheodoro/pontointeligente/api/PontoInteligenteApplication.java
+++ src/main/java/com/etheodoro/pontointeligente/api/PontoInteligenteApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PontoInteligenteApplication {
 
 	public static void main(String[] args) {
+		System.out.println("PontoInteligente");
 		SpringApplication.run(PontoInteligenteApplication.class, args);
 	}
 
